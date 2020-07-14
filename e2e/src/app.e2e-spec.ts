207162a32a1b7e8c608f8e3eaefad2f9a9e69c3b
lindawn <tan.lin.dawn@gmail.com>
2020-07-14 21:40:17 +0800

    fixed margins and title

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('cvAngular app is running!');
+    expect(page.getTitleText()).toEqual('cv-angular app is running!');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'cvAngular'`, () => {
+  it(`should have as title 'cv-angular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('cvAngular');
+    expect(app.title).toEqual('cv-angular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('cvAngular app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('cv-angular app is running!');
   });
 });
